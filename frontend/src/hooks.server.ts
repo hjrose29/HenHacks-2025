119fa35111837847de83f23de589dc8eddb35fdf
clxxiii <clxxiii@users.noreply.github.com>
2025-03-02 04:32:30 -0500

    docker configuration

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -5,13 +5,13 @@ export const handle: Handle = async ({ event, resolve }) => {
 
     const sessionToken = event.cookies.get("salus_session");
     if (sessionToken) {
-        const userReq = await fetch(`${env.PUBLIC_USER_SERVICE_URL}/api/users/me`);
+        const userReq = await fetch(`${env.PUBLIC_USER_SERVICE_URL}/api/users/me`, { credentials: "include", headers: { "Cookie": `salus_session=${sessionToken}` } });
+        console.log(userReq);
         if (userReq.status == 200) {
             const user = await userReq.json();
             if (user) {
                 event.locals.user = user;
             }
-            event.cookies.delete("salus_session", { path: "/" });
         }
     }
 
