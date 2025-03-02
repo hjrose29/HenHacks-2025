45e4e002b850d40f25e92b485deecb346c3c1461
will-s-stone <will@williamstone.me>
2025-03-01 19:32:34 -0500

    Working on the repository

--- backend/user-service/src/main/java/us/salus/userservice/controllers/AppHealthController.java
+++ backend/user-service/src/main/java/us/salus/userservice/controllers/AppHealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/write")
-public class HealthController {
+public class AppHealthController {
 
     /**
      * Basic health check endpoint that returns current system timestamp.
---
+++ backend/user-service/src/main/java/us/salus/userservice/repositories/UserRepository.java
@@ -0,0 +1,32 @@
+package us.salus.userservice.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+import us.salus.userservice.models.User;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByName(String name);
+
+    // Query to find users who have logged activities in a date range
+    @Query("{'historicalActivities.startDate': {$gte: ?0, $lte: ?1}}")
+    List<User> findUsersWithActivitiesBetweenDates(ZonedDateTime startDate, ZonedDateTime endDate);
+
+    // Query to find users who have not logged any meals
+    @Query("{'historicalMeals': {$size: 0}}")
+    List<User> findUsersWithNoMeals();
+
+    // Find users who have burned more than a specified number of calories
+    @Query("{'historicalCalories.caloriesBurned': {$gte: ?0}}")
+    List<User> findUsersByMinCaloriesBurned(float minCaloriesBurned);
+
+    // Find users who have consumed less than a specified number of calories
+    @Query("{'historicalCalories.caloriesConsumed': {$lte: ?0}}")
+    List<User> findUsersByMaxCaloriesConsumed(float maxCaloriesConsumed);
+}
